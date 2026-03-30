95c6691d975d83cd8c4447dcbc7e18f3d41fec69
Karen-Reis-Dev <karendosreis610@gmail.com>
2026-03-30 20:28:08 -0300

    Add files via upload

---
+++ aula 2003.py
@@ -0,0 +1,11 @@
+# i=int
+# for i in range (1,11):
+# print (i)
+# contador = 1
+# while contador <= 10 :
+#     print(contador)
+#     contador += 1
+contador=1
+while contador <= 10:
+     print (contador)
+     contador += 1
---
+++ introducao.py
@@ -0,0 +1,9 @@
+# print('hello, world') 
+# nome= 'Karen'
+# print('seu nome é:',nome)
+# nome= input('qual o seu nome?')
+# print('ola ' + nome + ',seja bem vindo(a)!')
+a = int(input('escolha um número:'))
+b = int(input('escolha outro número:'))
+soma = a + b
+print('a soma é', soma)
