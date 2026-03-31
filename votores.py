270fee3705872c108616ff35ec816b6dc07d429b
Karen-Reis-Dev <karendosreis610@gmail.com>
2026-03-30 21:52:41 -0300

    Add files via upload

---
+++ votores.py
@@ -0,0 +1,23 @@
+# numeros = [10,20,30,40,50]
+# print (numeros)
+# nome = 'Karen'
+# pet = 'cães'
+# print(nome, ',' ,pet)
+
+# valores = ['karen', 'cachorros']
+# print ('meu nome é',valores[0],'e tenho 9' ,valores[1], '.')
+# #uso de vetores
+
+# valores = ['karen', 'cachorros']
+# print (F"meu nome é {valores[0]} e tenho 9 {valores[1]}.")
+# #f-string
+
+valores = []
+print(input('digite um numero:'))
+print(valores)
+
+# vetor = []
+# for i in range(5):
+#     num = int(input('digite um número:'))
+#     vetor.append(num)
+# print('vetor:', vetor)
